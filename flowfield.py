8afc05c6848816eb06dd9a3be92b43678ad0390b
ElleStark <elle.stark5645@gmail.com>
2023-09-24 12:08:21 -0600

    refactored code for cleaner main script

--- flowfield.py
+++ flowfield.py
@@ -1,10 +1,14 @@
 """
 Classes for various flow map objects, including:
 - Double Gyre
+
+Above classes are subclasses of AnalyticalFlow which is subclass of FlowField
 """
 
 import numpy as np
 from math import *
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import scipy as sp
 
 
@@ -27,44 +31,50 @@ class FlowField:
         yout = y0 + (dt / 6) * (f1 + 2 * f2 + 2 * f3 + f4)
         return yout
 
+    def plot_trajectories(self, xlim, ylim):
+        """
+        Creates movie of particle trajectories as assigned in compute_flowmap method
+        Must call compute_flowmap before using this method.
+        :param xlim: x-axis limits in form [min, max]
+        :param ylim: y-axis limits in form [min, max]
+        :return: saves .mp4 animation of particle trajectories in /plots folder
+        """
 
-class DoubleGyre(FlowField):
+        x_trajs = self.trajectories[0]
+        y_trajs = self.trajectories[1]
+        # set up figure
+        fig, ax = plt.subplots()
+        # First snapshot
+        positions = ax.scatter(x_trajs, y_trajs, s=0.1, c='black')
+        # Plotting configuration
+        ax.set(xlim=xlim, ylim=ylim, xlabel='x', ylabel='y')
 
-    def __init__(self, a, epsilon, T_0, n):
+        def init_scatter():
+            positions.set_offsets([])
+            return(positions,)
+
+        def update(frame):
+            data = np.column_stack((x_trajs[frame], y_trajs[frame]))
+            positions.set_offsets(data)
+            return (positions, )
+
+        # len(DoubleGyre.trajectories[1]) for num frames?
+        traj_movie = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=200, blit=True)
+
+        # save video
+        f = r"plots/trajectories.mp4"
+        writervideo = animation.FFMpegWriter(fps=60)
+        traj_movie.save(f, writer=writervideo)
+
+class AnalyticalFlow(FlowField):
+
+    def __init__(self):
         super().__init__()
-
-        self.a = a  # velocity magnitude A aka U
-        self.epsilon = epsilon
-        self.T_0 = T_0
-        self.xvals = np.linspace(0, 2, num=n)
-        self.yvals = np.linspace(0, 1, num=int(n/2))
-        self.x, self.y = np.meshgrid(self.xvals, self.yvals, indexing='xy')
 
         # Attributes that can be defined by methods
         self.velocity_fields = None
         self.flow_map = None
         self.trajectories = None
-
-    def vfield(self, time, y):
-        """
-        Calculates velocity field based on double gyre analytical equations
-        :param x: scalar x value OR ndarray of x values
-        :param y: scalar y value OR ndarray of y values
-        :param time: scalar value for time
-        :return: list of u and v, where u is size x by y ndarray of horizontal velocity magnitudes,
-        and v is size x by y ndarray of vertical velocity magnitudes.
-        """
-        f = y[0]*[1+self.epsilon*np.sin(2*pi*time/self.T_0)*(y[0]-2)]
-        df = 1 + 2 * self.epsilon * (y[0] - 1) * np.sin(2 * pi * time / self.T_0)
-
-        u = -pi * self.a * np.sin(pi * f) * np.cos(pi * y[1])
-        v = pi * self.a * np.cos(pi * f) * np.sin(pi * y[1]) * df
-        u = np.squeeze(u)  # get rid of extra dimension of length 1 if present
-        v = np.squeeze(v)
-
-        vfield = np.array([u, v])  # convert to array for vectorization
-
-        return vfield
 
     def compute_vfields(self, t):
         """
@@ -92,7 +102,7 @@ class DoubleGyre(FlowField):
         Uses Runge Kutta 4th order method to find flow map from velocity field
         :return:
         """
-        dt = T/1000
+        dt = T / 1000
         L = abs(int(T / dt))  # need to calculate if dt definition is not based on T
         nx = len(self.xvals)
         ny = len(self.yvals)
@@ -124,4 +134,35 @@ class DoubleGyre(FlowField):
         self.flow_map = fmap
 
 
+class DoubleGyre(AnalyticalFlow):
 
+    def __init__(self, a, epsilon, T_0, n):
+        super().__init__()
+
+        self.a = a  # velocity magnitude A aka U
+        self.epsilon = epsilon
+        self.T_0 = T_0
+        self.xvals = np.linspace(0, 2, num=n)
+        self.yvals = np.linspace(0, 1, num=int(n / 2))
+        self.x, self.y = np.meshgrid(self.xvals, self.yvals, indexing='xy')
+
+    def vfield(self, time, y):
+        """
+        Calculates velocity field based on double gyre analytical equations
+        :param x: scalar x value OR ndarray of x values
+        :param y: scalar y value OR ndarray of y values
+        :param time: scalar value for time
+        :return: list of u and v, where u is size x by y ndarray of horizontal velocity magnitudes,
+        and v is size x by y ndarray of vertical velocity magnitudes.
+        """
+        f = y[0] * [1 + self.epsilon * np.sin(2 * pi * time / self.T_0) * (y[0] - 2)]
+        df = 1 + 2 * self.epsilon * (y[0] - 1) * np.sin(2 * pi * time / self.T_0)
+
+        u = -pi * self.a * np.sin(pi * f) * np.cos(pi * y[1])
+        v = pi * self.a * np.cos(pi * f) * np.sin(pi * y[1]) * df
+        u = np.squeeze(u)  # get rid of extra dimension of length 1 if present
+        v = np.squeeze(v)
+
+        vfield = np.array([u, v])  # convert to array for vectorization
+
+        return vfield
