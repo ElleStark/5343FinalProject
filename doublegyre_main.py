6e8167dead75870e1be77b75cbcbd84a62e75373
ElleStark <elle.stark5645@gmail.com>
2023-12-14 05:41:23 -0700

    begin particle tracking with diffusion

--- doublegyre_main.py
+++ doublegyre_main.py
@@ -17,7 +17,7 @@ eps = 0.25
 T_0 = 10
 
 # Create double gyre object and calculate velocity fields
-n = 400  # number of grid steps in the x direction, use fewer when plotting velocity arrows
+n = 1000  # number of grid steps in the x direction, use fewer when plotting velocity arrows
 DoubleGyre = flowfield.DoubleGyre(a, eps, T_0, n)
 
 
@@ -29,12 +29,15 @@ DoubleGyre = flowfield.DoubleGyre(a, eps, T_0, n)
 #    plt.quiver(*DoubleGyre.velocity_fields[time*T_0])
 #    plt.show()
 
+
+# FTLE FIELD CALCULATIONS & MOVIE
+
 # Find flow map using Runge-Kutta 4th order method to integrate backwards from t = t0 to t = t0-T
 T = -2*T_0  # integration time - Pratt et al. used 2-2.5 turnover times
 tau_list = np.linspace(0, T_0, 100)  # evolution times
-start_time = time.time()
-DoubleGyre.compute_flow_map(T, tau_list, dt=T/50, method='RK4')
-print('time to compute trajectories is: ' + str(time.time()-start_time))
+#start_time = time.time()
+#DoubleGyre.compute_flow_map(T, tau_list, dt=T/50, method='IE')
+#print('time to compute trajectories is: ' + str(time.time()-start_time))
 
 # If only interested in one or a few points in time, can use method 'compute_flow_map_w_trajs' and plot trajectories
 # tau_list = [0]
@@ -42,16 +45,27 @@ print('time to compute trajectories is: ' + str(time.time()-start_time))
 # DoubleGyre.plot_trajectories([0, 2], [0, 1])
 
 # Compute FTLE using central differencing for strain tensor
-start_time = time.time()
-DoubleGyre.compute_ftle()
-print('time to compute FTLE is: ' + str(time.time()-start_time))
+#start_time = time.time()
+#DoubleGyre.compute_ftle()
+#print('time to compute FTLE is: ' + str(time.time()-start_time))
 
 # Create movie of FTLE field, passing in xlim and ylim for plotting. Saves ftle.mp4 in \plots\ folder.
-DoubleGyre.ftle_movie((0, 2), (0, 1))
+#DoubleGyre.ftle_movie((0, 2), (0, 1))
 
 # Plot FTLE field at a single point in time. Saves ftle_snap.png in \plots\ folder.
 # DoubleGyre.ftle_snapshot(tau_list[0])
 
 # Save individual FTLE field data if desired
 # np.savetxt('data/doublegyre_negftle_t2.5T_T2T0_pratt.txt', DoubleGyre.ftle[tau_list[1]])
+
+
+# PARTICLE TRACKING MODEL & MOVIE
+
+np = 100000  # number of particles initialized in each location
+dt = T/50
+D = 1
+
+DoubleGyre.track_particles_rw()
+
+
 
